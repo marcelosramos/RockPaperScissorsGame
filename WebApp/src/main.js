d2389a5695f9e8a1199094041829cf533f5dba5c
Marcelo Ramos <marcelosramos@gmail.com>
2018-04-04 19:25:48 -0300

    api url fixing 2

--- WebApp/src/main.js
+++ WebApp/src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
-let ROOT_URL = process.env.API_URL || 'http://localhost:49445';
+let ROOT_URL = process.env.API_URL || 'http://matchmanagerapi.azurewebsites.net';
 //ROOT_URL = 'http://localhost:49445';
 const API_URL = `${ROOT_URL}/api`;
 const NEW_PLAYER_URL = `${API_URL}/player`;
